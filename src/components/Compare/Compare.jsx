a2e3d0b6b9ec1c080891d77a80a405b1a4a498ed
AzizKri <aziz.kanakri@gmail.com>
2024-03-02 13:18:11 +0400

    Create Compare component

---
+++ src/components/Compare/Compare.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './compare.css';
+
+function Compare() {
+    return(
+        <div className="Compare">
+            Compare
+        </div>
+    )
+}
+
+export default Compare
