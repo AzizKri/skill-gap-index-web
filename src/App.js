f0d888c912a4ea6dd42da68c362d0b0efdebd485
AzizKri <aziz.kanakri@gmail.com>
2024-03-02 13:18:49 +0400

    Add particles & use components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,31 @@
-import React, { useState } from "react";
-import { ColorRing } from 'react-loader-spinner';
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import particlesOptions from "./particles.json";
 import './App.css';
+import Upload from "./components/Upload/Upload";
+import Compare from "./components/Compare/Compare";
 
 function App() {
-    const [university, setUniversity] = useState("0");
-    const [file, setFile] = useState(null);
-    const [uploadStatus, setUploadStatus] = useState(0);
+    const [init, setInit] = useState(false);
+    const pathname = window.location.pathname;
 
-    const onClick = async (e) => {
-        e.preventDefault();
-        await upload();
-    }
-
-    const upload = async (e) => {
-        setUploadStatus(1)
-        const requestOptions = {
-            method: 'PUT',
-            headers: {
-                "Authorization": "Bearer " + process.env.REACT_APP_AUTH_TOKEN,
-                "file-name": `${university}/${Date.now() + '_' + file.name}`
-            },
-            body: file
-        };
-        await fetch(`https://sgi-upload.uaeu.club/`, requestOptions)
-        .catch((e) => {
-            console.log(`Error ${e}`)
-        })
-        setUploadStatus(2)
-    }
-
+    // Particles (BG) engine
+    useEffect(() => {
+        if (init) {
+            return;
+        }
+        initParticlesEngine(async (engine) => {
+            await loadFull(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, [init]);
+    
     return (
-        <div className="App">
-            <form className="Form" onSubmit={onClick}>
-                <label htmlFor="university">Select a university: </label>
-                <select onChange={(e) => {setUniversity(e.target.value)}}>
-                    <option value="0">Select</option>
-                    <option value="UAEU">United Arab Emirates University</option>
-                </select>
-                <br /><br />
-                <label htmlFor="file">Upload file: </label>
-                <input type="file" id="file" name="upload" onChange={(e) => {setFile(e.target.files[0])}}/>
-                <br /><br />
-                <input type="submit" disabled={(university === "0" || file == null || uploadStatus !== 0)? true : false} />
-            </form>
-            {(uploadStatus === 1)? <ColorRing height="64" width="64" colors={[]} /> :
-            (uploadStatus === 2)? <h2>Uploaded!</h2> :
-            null}
+        <div className="App" >
+            {init && <Particles options={particlesOptions} />}
+            {(pathname.toLowerCase() === '/compare')? <Compare /> : <Upload />}
         </div>
     );
 }
