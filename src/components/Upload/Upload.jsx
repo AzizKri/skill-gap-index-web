e3d58fdbe3cf38eb2a281ff823de182527876e5a
AzizKri <aziz.kanakri@gmail.com>
2024-03-10 20:45:30 +0400

    Redesign

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ColorRing } from 'react-loader-spinner';
+import { AiOutlineFile } from "react-icons/ai";
 import './upload.css';
 
 function Upload() {
@@ -23,30 +24,50 @@ function Upload() {
             body: file
         };
         await fetch(`https://sgi-upload.uaeu.club/`, requestOptions)
-        .catch((e) => {
-            console.log(`Error ${e}`)
-        })
+            .catch((e) => {
+                console.log(`Error ${e}`)
+            })
         setUploadStatus(2)
     }
 
     return (
-        <div className="upload" >
+        <div className="content" >
+            <div className="content-location">
+                Home / Upload
+            </div>
+            <p className="title">Upload</p>
             <div className="upload_card">
                 <form className="upload_card_form" onSubmit={onClick}>
-                    <label htmlFor="university">Select a university: </label>
-                    <select onChange={(e) => {setUniversity(e.target.value)}}>
-                        <option value="0">Select</option>
-                        <option value="UAEU">United Arab Emirates University</option>
-                    </select>
+                    <div className="upload_card_form_select">
+                        <label htmlFor="university">Select a university: </label>
+                        <select onChange={(e) => { setUniversity(e.target.value) }}>
+                            <option value="0">Select</option>
+                            <option value="UAEU">United Arab Emirates University</option>
+                        </select>
+                    </div>
                     <br />
-                    <label htmlFor="file">Upload file: </label>
-                    <input type="file" id="file" name="upload" onChange={(e) => {setFile(e.target.files[0])}}/>
+                    <div className="upload_card_form_upload">
+                        <label htmlFor="file">Upload file: </label>
+                        <label className="upload_card_form_upload_input" >
+                            <input type="file" id="file" name="upload" onChange={(e) => { setFile(e.target.files[0]) }} />
+                            Select
+                        </label>
+                    </div>
                     <br />
-                    <input type="submit" disabled={(university === "0" || file == null || uploadStatus !== 0)? true : false} />
+                    {file? 
+                    <div>
+                        <div className="upload_card_form_files">
+                            <AiOutlineFile size={18}/>
+                            <span>{file.name}</span>
+                        </div>
+                        <br />
+                    </div> :
+                    null}
+                    <input className="upload_card_form_submit" type="submit" disabled={(university === "0" || file == null || uploadStatus !== 0) ? true : false} />
                 </form>
-                {(uploadStatus === 1)? <ColorRing height="64" width="64" colors={[]} /> :
-                (uploadStatus === 2)? <h3>Uploaded!</h3> :
-                null}
+                {(uploadStatus === 1) ? <ColorRing height="64" width="64" colors={[]} /> :
+                    (uploadStatus === 2) ? <h3>Uploaded!</h3> :
+                        null}
             </div>
         </div>
     );
